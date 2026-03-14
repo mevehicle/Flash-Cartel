329f0cad98729ca5e4a9a9b7a44efddf936c178d
Rene Thomas <U0568426@unimail.hud.ac.uk>
2026-03-14 22:39:56 +0000

    Login system works except the error messages

---
+++ Flash-Cartel-main/Code/includes/login.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+// Backend to login.php
+
+// Check if user has submitted form.
+if (isset($_POST["submit"])) {
+
+  // Has user filled in all fields of form?
+  if (!array_key_exists("username", $_POST) || !array_key_exists("pwd", $_POST)) {
+    header("location: ../index.php?error=emptyinput");
+    exit();
+  }
+
+  $username = trim($_POST["username"]);
+  $password = trim($_POST["pwd"]);
+
+  require_once 'functions.inc.php';
+
+  loginUser($username, $password);
+
+  // If user tries to access this page without submitting form, send them back to login page.
+} else {
+  header("location: ../login.php");
+  exit();
+}
---
+++ Flash-Cartel-main/Code/includes/register.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+// Backend to register.php form
+
+if (isset($_POST["submit"])) {
+  $username = trim($_POST["username"]);
+  $email = trim($_POST["email"]);
+  $password = trim($_POST["pwd"]);
+  $passwordRepeat = trim($_POST["pwdRepeat"]);
+
+  require 'functions.inc.php';
+
+  // Has user filled in all fields of form?
+  if (emptyInputSignup($username, $email, $password, $passwordRepeat) !== false) {
+    header("location: ../register.php?error=emptyinput");
+    exit();
+  }
+
+  // Check if username is valid and doesn't already exist in database.
+  if (invalidUid($username) !== false) {
+    header("location: ../register.php?error=invaliduid");
+    exit();
+  }
+
+  // Check if email is valid and doesn't already exist in database.
+  if (invalidEmail($email) !== false) {
+    header("location: ../register.php?error=invalidemail");
+    exit();
+  }
+
+  // Check if passwords match.
+  if (pwdMatch($password, $passwordRepeat) !== false) {
+    header("location: ../register.php?error=passwordsdontmatch");
+    exit();
+  }
+
+  // Check if password is strong enough.
+  if (pwdNotStrong($password) !== false) {
+    header("location: ../register.php?error=passwordnotstrong");
+    exit();
+  }
+
+  // Check if username is already taken.
+  if (uidExists($conn, $username) !== false) {
+    header("location: ../register.php?error=usernametaken");
+    exit();
+  }
+
+  // If all the above functions return false, create user in database.
+  createUser($username, $email, $password);
+  header("location: ../register.php?error=none");
+  exit();
+} else {
+  // If user tries to access this page without submitting form, send them back to registration page.
+  header("location: ../register.php");
+}
